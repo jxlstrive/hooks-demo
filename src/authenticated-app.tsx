73c51cd3b22db35ee8cfca77ef6639b787ac9a68
jiaxiaoli <jxlstrive@gmail.com>
2022-03-13 18:31:41 +0800

    style: use Row component layout the page

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { useAuth } from 'context/auth-context'
 import { ProjectListScreen } from 'screens/project-list'
+import { Row } from 'components/lib'
 
 import styled from '@emotion/styled'
 /**
@@ -17,11 +18,12 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth()
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>Logo</h3>
-          <h3>项目</h3>
-          <h3>用户</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+          {/* <HeaderItem as={'div'}>another</HeaderItem> */}
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
@@ -53,6 +55,10 @@ export const AuthenticatedApp = () => {
 // `;
 
 
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`
+
 /* grid-template-rows 几行 高度。6rem calc(100vh - 6rem) auto; 等于 6rem 1fr 6rem;  1fr 即剩余高度自适应，就相当于 100vh - 12rem */
 /* grid-template-columns 几列。宽度*/
 /* grid-template-areas 排列 */
@@ -71,17 +77,10 @@ const Container = styled.div`
 `;
 
 // grid-area 用来给 grid 子元素起名字
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+const Header = styled(Row)`
 `;
 
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
+const HeaderLeft = styled(Row)`
 `;
 const HeaderRight = styled.div``; 
 const Main = styled.main`

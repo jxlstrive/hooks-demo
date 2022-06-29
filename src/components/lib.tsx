2c132ffa95053fc3fddd6abc7899b27e16c54392
jiaxiaoli <jxlstrive@gmail.com>
2022-06-29 17:51:11 +0800

    feat: handling 'me' interface loading and error

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,20 +1,47 @@
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 
 /**
- * > * {} 在其下边的直接子元素 
+ * > * {} 在其下边的直接子元素
  */
 export const Row = styled.div<{
-  gap?: number | boolean,
-  between?: boolean,
-  marginBottom?: number
-  }>`
+  gap?: number | boolean;
+  between?: boolean;
+  marginBottom?: number;
+}>`
   display: flex;
   align-items: center;
-  justify-content: ${props => props.between ? 'space-between' : undefined};
-  margin-bottom: ${props => props.marginBottom + 'rem'};
+  justify-content: ${(props) => (props.between ? "space-between" : undefined)};
+  margin-bottom: ${(props) => props.marginBottom + "rem"};
   > * {
     margin-top: 0 !important;
     margin-bottom: 0 !important;
-    margin-right: ${props => typeof props.gap === 'number' ? props.gap + 'rem' : props.gap ? '2rem' : undefined};
+    margin-right: ${(props) =>
+      typeof props.gap === "number"
+        ? props.gap + "rem"
+        : props.gap
+        ? "2rem"
+        : undefined};
   }
-`
+`;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () => (
+  <FullPage>
+    <Spin size={"large"} />
+  </FullPage>
+);
+
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => (
+  <FullPage>
+    <DevTools />
+    <Typography.Text type={"danger"}>{error?.message}</Typography.Text>
+  </FullPage>
+);
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,59 +1,88 @@
-import React, { ReactNode, useState } from 'react'
+import React, { ReactNode, useState } from "react";
 
-import { useMount } from 'utils'
-import { http } from 'utils/http'
-import * as auth from 'auth-provider'
-import { User } from 'screens/project-list/search-panel'
-
+import { useMount } from "utils";
+import { http } from "utils/http";
+import * as auth from "auth-provider";
+import { User } from "screens/project-list/search-panel";
+import { useAsync } from "utils/use-async";
+import { FullPageErrorFallback, FullPageLoading } from "components/lib";
 interface AuthForm {
-  username: string,
-  password: string
+  username: string;
+  password: string;
 }
 
 /**
  * 需要在页面刷新的时候，能够初始化 user（原因：登录成功，页面刷新之后，变为未登录之前的页面）
  * 取自 localStorage 中的 token，从而获取 user 的信息，找到之后再将其赋值给 setUser
- * 
- * @return {*} 
+ *
+ * @return {*}
  */
 const bootstrapUser = async () => {
-  let user = null
-  const token = auth.getToken()
+  let user = null;
+  const token = auth.getToken();
   if (token) {
-    const data = await http('me', { token })
-    user = data.user
+    const data = await http("me", { token });
+    user = data.user;
   }
-  return user
-}
+  return user;
+};
 
-const AuthContext = React.createContext<{
-  user: User | null,
-  register: (form: AuthForm) => Promise<void>,
-  login: (form: AuthForm) => Promise<void>,
-  logout: () => Promise<void>
-} | undefined>(undefined)
-AuthContext.displayName = 'AuthContext'  // 主要用在 DevTools 里面
+const AuthContext = React.createContext<
+  | {
+      user: User | null;
+      register: (form: AuthForm) => Promise<void>;
+      login: (form: AuthForm) => Promise<void>;
+      logout: () => Promise<void>;
+    }
+  | undefined
+>(undefined);
+AuthContext.displayName = "AuthContext"; // 主要用在 DevTools 里面
 
-export const AuthProvider = ({children}: {children: ReactNode}) => {
-  const [user, setUser] = useState<User | null>(null)
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  // const [user, setUser] = useState<User | null>(null)
+
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
 
   // 函数编程里边的 point free 即数学里边的消参（.then(user => setUser(user)) 相当于 .then(setUser)）
-  const login = (form: AuthForm) =>  auth.login(form).then(setUser)
-  const register = (form: AuthForm) => auth.register(form).then(user => setUser(user))
-  const logout = () => auth.logout().then(() => setUser(null))
+  const login = (form: AuthForm) => auth.login(form).then(setUser);
+  const register = (form: AuthForm) =>
+    auth.register(form).then((user) => setUser(user));
+  const logout = () => auth.logout().then(() => setUser(null));
 
   // 当整个 auth Provider 加载，也就是说整个 app 加载的时候，使用 useMount，调用 bootstrapUser
   useMount(() => {
-    bootstrapUser().then(setUser)
-  })
+    // bootstrapUser().then(setUser)
+    run(bootstrapUser());
+  });
 
-  return <AuthContext.Provider children={children} value={{ user, login, register, logout }} />
-}
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
+
+  return (
+    <AuthContext.Provider
+      children={children}
+      value={{ user, login, register, logout }}
+    />
+  );
+};
 
 export const useAuth = () => {
-  const context = React.useContext(AuthContext)
-  if(!context) {
-    throw new Error('useAuth 必须在 AuthProvider 中使用')
+  const context = React.useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth 必须在 AuthProvider 中使用");
   }
-  return context
-}
+  return context;
+};
